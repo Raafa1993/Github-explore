e24e2aef53668abee3a38c3893b864ff33f2b426
Raafa1993 <rafaelajds@gmail.com>
2020-07-15 15:59:12 -0300

    estilizacao dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositorios no Github</Title>;
+  return (
+  <>
+    <img src={logoImg} alt="Github Explore"/>
+    <Title>Explore repositorios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositorio" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+      <img src="https://avatars0.githubusercontent.com/u/37309024?s=460&u=a3fa5c6a24da1e1369bfe192e547223ae595f3cf&v=4"
+           alt="Rafael Araujo"
+      />
+      <div>
+        <strong>Raafa1993/NextLevelWeek</strong>
+        <p>Ecoleta é uma forma de conectar empresas e entidades</p>
+      </div>
+
+      <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+      <img src="https://avatars0.githubusercontent.com/u/37309024?s=460&u=a3fa5c6a24da1e1369bfe192e547223ae595f3cf&v=4"
+           alt="Rafael Araujo"
+      />
+      <div>
+        <strong>Raafa1993/NextLevelWeek</strong>
+        <p>Ecoleta é uma forma de conectar empresas e entidades</p>
+      </div>
+
+      <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+      <img src="https://avatars0.githubusercontent.com/u/37309024?s=460&u=a3fa5c6a24da1e1369bfe192e547223ae595f3cf&v=4"
+           alt="Rafael Araujo"
+      />
+      <div>
+        <strong>Raafa1993/NextLevelWeek</strong>
+        <p>Ecoleta é uma forma de conectar empresas e entidades</p>
+      </div>
+
+      <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+  );
 };
 
 export default Dashboard;
